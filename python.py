2030d4182596ad336aa8867b42bcdb3884059a27
Yu Jo <zoey5538@gmail.com>
2018-03-01 18:58:23 +0000

    comments

--- python.py
+++ python.py
@@ -1,12 +1,12 @@
 f = open('a_example.in')
-yourList = f.readlines()
+yourList = f.readlines() # returns a list of lines
 # yourList = [line.rstrip('\n') for line in f]
 
-details = yourList[:]
-details = details[:1]
-rides = yourList[1:]
+details = yourList[:] # copy
+details = details[:1] # first element(line) (string)
+rides = yourList[1:] # the rest
 
-details = details[0].split()
+details = details[0].split() # list
 
 print 'details'
 print details
@@ -15,4 +15,3 @@ print 'rides'
 for a in rides:
 	a = a.split()
 	print a
-
