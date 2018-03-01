07957637e3eb0e8adaaff37cd63f0ca0b8160d42
Mayank Gupta <mayank@guptamayank.co.uk>
2018-03-01 18:48:24 +0000

    converted input to lists

--- python.py
+++ python.py
@@ -2,5 +2,17 @@ f = open('a_example.in')
 yourList = f.readlines()
 # yourList = [line.rstrip('\n') for line in f]
 
-for a in yourList:
+details = yourList[:]
+details = details[:1]
+rides = yourList[1:]
+
+details = details[0].split()
+
+print 'details'
+print details
+
+print 'rides'
+for a in rides:
+	a = a.split()
 	print a
+
